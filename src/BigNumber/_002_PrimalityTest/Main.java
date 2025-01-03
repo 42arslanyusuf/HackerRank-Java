d2b17e5ddd77f4ca54c734b2f999165b87db219b
42arslan.yusuf <42arslan.yusuf@gmail.com>
2025-01-03 10:45:29 +0300

    primality test

---
+++ src/BigNumber/_002_PrimalityTest/Main.java
@@ -0,0 +1,23 @@
+package BigNumber._002_PrimalityTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+
+        BigInteger number = new BigInteger(n);
+
+        if (number.isProbablePrime(10)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+}
